4d4601db43d69d2db34303992e5cd707ce1a8a6f
calling0108 <74917625+calling0108@users.noreply.github.com>
2024-09-04 21:44:01 +0900

    둣ㄷㄱ

--- section/sec25_DOM events/271 Bubbling/app.js
+++ section/sec25_DOM events/271 Bubbling/app.js
@@ -1,10 +1,13 @@
 const button = document.querySelector('#changeColor');
 const container = document.querySelector('#container');
 
+// 이벤트 버블링을 멈추는 방법.
+// function()을 function(e)로 변경
 button.addEventListener('click', function (e) {
     container.style.backgroundColor = makeRandColor();
+    // 이벤트가 더이상 버블링하지 않도록 막아줌
     e.stopPropagation();
-})
+})``
 container.addEventListener('click', function () {
     container.classList.toggle('hide');
 })
