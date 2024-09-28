939fd33a2a75b6a07667ce1d0072ba8100031eba
calling0108 <74917625+calling0108@users.noreply.github.com>
2024-09-28 13:10:45 +0900

    enter

--- section/sec26_Score Keeper/script.js
+++ section/sec26_Score Keeper/script.js
@@ -7,41 +7,53 @@ const winningScoreSelect = document.querySelector('#playto');
 
 let p1Score = 0;
 let p2Score = 0;
-let winningScore = 5;
+let winningScore = 3;
 let isGameOver = false;
 
+// ✅ p1 버튼
 p1Button.addEventListener('click', function () {
+    // ✅ 게임이 끝날 경우 if문 실행 x
     if (!isGameOver) {
         p1Score += 1;
         if (p1Score === winningScore) {
             isGameOver = true;
+            // ✅ classList로 html <style> 태그를 control
+            p1Display.classList.add('winner');
+            p2Display.classList.add('loser');
         }
         p1Display.textContent = p1Score;
     }
 })
 
+// ✅ p2 버튼
 p2Button.addEventListener('click', function () {
     if (!isGameOver) {
         p2Score += 1;
         if (p2Score === winningScore) {
             isGameOver = true;
+            p2Display.classList.add('winner');
+            p1Display.classList.add('loser');
         }
         p2Display.textContent = p2Score;
     }
 })
 
+// ✅ 점수가 select될 때마다 이벤트 발생, reset 진행
 winningScoreSelect.addEventListener('change', function () {
-    alert(winningScore);
     winningScore = parseInt(this.value);
+    // alert(winningScore);
     reset();
 })
 
 resetButton.addEventListener('click', reset)
 
+// ✅ 점수 초기화
 function reset() {
     isGameOver = false;
     p1Score = 0;
     p2Score = 0;
     p1Display.textContent = 0;
     p2Display.textContent = 0;
+    p1Display.classList.remove('winner', 'loser');
+    p2Display.classList.remove('winner', 'loser');
 }
