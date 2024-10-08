20dee58381526d7576bb3a32880ad5febf31d524
calling0108 <74917625+calling0108@users.noreply.github.com>
2024-10-09 00:21:37 +0900

    enter

---
+++ section/sec27_비동기식 JS/281 Demo1 faceRequest use Callback/script.js
@@ -0,0 +1,36 @@
+
+// ✅가짜 요청 콜백
+const fakeRequestCallback = (url, success, failure) => {
+    const delay = Math.floor(Math.random() * 4500) + 500;
+    setTimeout(() => {
+        if (delay > 4000) {
+            failure('Connection Timeout :(')
+        } else {
+            success(`Here is your fake data from ${url}`)
+        }
+    }, delay)
+}
+
+
+const fakeRequestPromise = (url) => {
+    return new Promise((resolve, reject) => {
+        const delay = Math.floor(Math.random() * (4500)) + 500;
+        setTimeout(() => {
+            if (delay > 4000) {
+                reject('Connection Timeout :(')
+            } else {
+                resolve(`Here is your fake data from ${url}`)
+            }
+        }, delay)
+    })
+}
+
+
+fakeRequestCallback('books.com',
+    function (response) {
+        console.log("IT WORKED!!!!");
+        console.log(response);
+    },
+    function (err) {
+        console.log("ERROR!!!", err);
+    })
---
+++ 개념정리_함수선언식vs함수표현식.js
@@ -0,0 +1,9 @@
+
+// 🔸함수-표현식-VS-함수-선언식
+// https://velog.io/@bisu8018/%ED%95%A8%EC%88%98-%ED%91%9C%ED%98%84%EC%8B%9D-VS-%ED%95%A8%EC%88%98-%EC%84%A0%EC%96%B8%EC%8B%9D
+
+// 🔸모던 자바스크립트에서 호이스팅(Hoisting)
+// https://velog.io/@bisu8018/hoisting-in-modern-javascript
+
+// 🔸자바스크립트의 실행 컨텍스트(Execute Context)와 실행 스택(Execute Stack) 이해하기
+// https://velog.io/@bisu8018/Understanding-Execution-Context-and-Execution-Stack-in-Javascript
