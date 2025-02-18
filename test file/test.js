316b0b98ad529f97d02da0906c139f6993a60169
calling0108 <74917625+calling0108@users.noreply.github.com>
2025-02-18 18:06:41 +0900

    enter

--- test file/test.js
+++ test file/test.js
@@ -1,40 +1,8 @@
-var x = "global";
+'use strict';
 
 function foo() {
-    var x = "local";
+    x = 10;
     console.log(x);
-    var b = "test"
-    return console.log(b);
 }
 
 foo();
-console.log(x);
-
-
-
-
-
-const person = {
-    firstName: "Ungmo",
-    lastName: "Lee",
-
-    get fullName() {
-        return `${this.firstName} ${this.lastName}`;
-    },
-
-    set fullName(name) {
-        [this.firstName, this.lastName] = name.split(' ');
-    }
-};
-
-console.log(person.firstName + ' ' + person.lastName);
-
-person.fullName = "Heegun Lee";
-console.log(person);
-console.log(person.fullName);
-
-let descriptor = Object.getOwnPropertyDescriptor(person, "firstName");
-console.log(descriptor)
-
-descriptor = Object.getOwnPropertyDescriptor(person, "fullName");
-console.log(descriptor);
